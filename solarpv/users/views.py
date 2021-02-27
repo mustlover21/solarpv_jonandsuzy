c59ad1d4a1d5107c90afa084fdbedb67eb71b668
Suzy Kaufman <skaufma5@asu.edu>
2021-02-27 03:29:18 -0700

    still works -.SK

--- solarpv/users/views.py
+++ solarpv/users/views.py
@@ -1,17 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import User
 from .forms import Userform
 from users.models import User
+
 
 def register_view(request):
     if request.method == "POST":
         form = Userform(request.POST or None)
         if form.is_valid():
             form.save()
-        return render(request, 'users/register.html', {})
+        return redirect(request, 'users:login')
     else:
-        return render(request, 'users/register.html', {})
+        form = Userform()
+    return render(request, 'users/register.html', {})
 
 
 def login_view(request):
-    return render(request, 'users/login.html', {})
+    return render(request,'users/login.html', {})
